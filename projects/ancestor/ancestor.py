0dc4b3846c1fd1817151a3ea6bb09d94bd6c5236
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-17 13:23:44 +0000

    add earliest ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,15 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def get_parent(ancestors, starting_node):
+    for (parent, child) in ancestors:
+        if child == starting_node:
+            return parent
+
+def earliest_ancestor(ancestors, starting_node, iterations = 0):
+    if get_parent(ancestors, starting_node) is None:
+        if iterations == 0:
+            return -1
+        else:
+            return starting_node
+    else:
+        iterations += 1
+        return earliest_ancestor(ancestors, get_parent(ancestors, starting_node), iterations)
