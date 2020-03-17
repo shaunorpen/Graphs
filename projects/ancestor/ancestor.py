32b5f73a619dce8a8f56782b361dd9be57855134
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-17 14:15:27 +0000

    minor refactor to make more efficient

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -5,11 +5,12 @@ def get_parent(ancestors, starting_node):
             return parent
 
 def earliest_ancestor(ancestors, starting_node, iterations = 0):
-    if get_parent(ancestors, starting_node) is None:
+    parent = get_parent(ancestors, starting_node)
+    if parent is None:
         if iterations == 0:
             return -1
         else:
             return starting_node
     else:
         iterations += 1
-        return earliest_ancestor(ancestors, get_parent(ancestors, starting_node), iterations)
+        return earliest_ancestor(ancestors, parent, iterations)
